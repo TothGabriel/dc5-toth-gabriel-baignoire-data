aca2e79bc9a0427ccf3e5527344aeaa1feb9dadf
GabrielToth <gabt71@gmail.com>
2023-11-06 08:30:47 +0100

    ex1-b

--- ex1-b.py
+++ ex1-b.py
@@ -1,12 +1,34 @@
 # Demander à l'utilisateur de saisir une phrase
 phrase = input("Veuillez saisir une phrase : ")
 
-# Afficher la phrase en majuscules
-print("Phrase en majuscules:", phrase.upper())
+# Convertir la phrase en majuscules sans utiliser .upper()
+phrase_majuscules = ''
+for caractere in phrase:
+    if 'a' <= caractere <= 'z':  # Vérifier si c'est une lettre minuscule
+        phrase_majuscules += chr(ord(caractere) - 32)  # Convertir en majuscule
+    else:
+        phrase_majuscules += caractere
 
-# Afficher la phrase en minuscules
-print("Phrase en minuscules:", phrase.lower())
+# Convertir la phrase en minuscules sans utiliser .lower()
+phrase_minuscules = ''
+for caractere in phrase:
+    if 'A' <= caractere <= 'Z':  # Vérifier si c'est une lettre majuscule
+        phrase_minuscules += chr(ord(caractere) + 32)  # Convertir en minuscule
+    else:
+        phrase_minuscules += caractere
 
-# Calculer et afficher le nombre de mots
-nombre_de_mots = len(phrase.split())
+# Compter le nombre de mots sans utiliser .split()
+nombre_de_mots = 0
+dans_mot = False
+for caractere in phrase:
+    if caractere.isalpha():  # Vérifier si le caractère est une lettre
+        if not dans_mot:
+            nombre_de_mots += 1
+            dans_mot = True
+    else:
+        dans_mot = False
+
+# Afficher les résultats
+print("Phrase en majuscules:", phrase_majuscules)
+print("Phrase en minuscules:", phrase_minuscules)
 print("Nombre de mots dans la phrase:", nombre_de_mots)
