22a6abbb4b167f905555f1723a7a2758862157e8
GabrielToth <gabt71@gmail.com>
2023-11-06 08:24:06 +0100

    ex1-a

---
+++ ex1-a.py
@@ -0,0 +1,10 @@
+# Création des variables
+nom_entreprise = "Entreprise"
+nombre_clients = 150
+revenu_moyen_par_client = 200.50  # en euros ou en dollars, par exemple
+
+# Calcul du revenu total
+revenu_total = nombre_clients * revenu_moyen_par_client
+
+# Affichage du revenu total
+print(f"Le revenu total pour {nom_entreprise} est de {revenu_total} euros.")
